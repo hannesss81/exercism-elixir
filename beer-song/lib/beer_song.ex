50f256208a721ee8dffa7a7dbbfde338ae9a0c1d
hannesss81 <hannesss81@gmail.com>
2020-08-10 09:16:15 +0300

    beer-song refactor

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -5,25 +5,45 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(number) do
     case number do
-      2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-      1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-      0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      n -> "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+      2 -> two()
+      1 -> one()
+      0 -> zero()
+      _ -> several(number)
     end
+  end
+
+  defp two do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  defp one do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  defp zero do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  defp several(n) do
+    """
+    #{n} bottles of beer on the wall, #{n} bottles of beer.
+    Take one down and pass it around, #{n - 1} bottles of beer on the wall.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    range
-    |> Enum.map(&(verse(&1)))
-    |> Enum.join("\n")
-  end
-
-  def lyrics() do
-    lyrics(99..0)
-  end
+  def lyrics(range \\ 99..0), do: Enum.map_join(range, "\n", &(verse(&1)))
 
 end
